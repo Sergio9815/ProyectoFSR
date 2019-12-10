ce0039f294cc6214661e7f2d29e4b6cb3d71bd40
Sergio9815 <sergio.gonzalez3@utp.ac.pa>
2019-12-09 21:36:34 -0500

    first commit

--- sketch_nov08a/sketch_nov08a.ino
+++ sketch_nov08a/sketch_nov08a.ino
@@ -4,7 +4,7 @@
 
 //Movimientos
 #define ADELANTE  {digitalWrite(IN1,HIGH);digitalWrite(IN2,LOW);digitalWrite(IN3,LOW);digitalWrite(IN4,HIGH);}   
-#define ATRAS     {digitalWrite(IN1,LOW);digitalWrite(IN2,HIGH);digitalWrite(IN3,LOW);digitalWrite(IN4,HIGH);}                         
+#define ATRAS     {digitalWrite(IN1,LOW);digitalWrite(IN2,HIGH);digitalWrite(IN3,HIGH);digitalWrite(IN4,LOW);}                         
 #define GIROI     {digitalWrite(IN1,LOW);digitalWrite(IN2,HIGH);digitalWrite(IN3,LOW);digitalWrite(IN4,HIGH);}   
 #define GIROD     {digitalWrite(IN1,HIGH);digitalWrite(IN2,LOW);digitalWrite(IN3,HIGH);digitalWrite(IN4,LOW);}  
 #define IZQUIERDA {digitalWrite(IN1,LOW);digitalWrite(IN2,HIGH);digitalWrite(IN3,HIGH);digitalWrite(IN4,HIGH);}        
@@ -29,24 +29,33 @@ int dis = 0;
 
 //VARIABLES DE BÚSQUEDA CON SERVO
 boolean go = false;
-boolean ok;
-boolean ok2;
-boolean ok3;
+boolean ok = false;
+boolean ok2 = false;
+boolean ok3 = false;
 
 //VARIBALES PARA LAS FASES
 boolean fase1 = true;
 boolean fase2 = false;
+boolean fase3 = false;
 
 
 //SERVO MOTORES
 Servo myservo, servoA;  // crea el objeto servo
 
 //LEDS
-int pinVerde = 11;
-int pinRojo = 2;
+int pinVerde = A1;
+int pinRojo = A2;
+
+//INFRARROJO
+int infraIZQ = 2;    // izquierdo - pin del infrarrojos utilizado como entrada digital
+int infraDER = 11;  //  derecho
+int valorInfraI = 0;  // Valor inicial de la lectura digital del infrarrojos izquierdo 
+int valorInfraD = 0;  // derecho
 
 void setup()
 {  
+    pinMode(infraIZQ, INPUT);    
+    pinMode(infraDER, INPUT); 
     pinMode(pinVerde, OUTPUT);
     pinMode(pinRojo, OUTPUT);
     myservo.attach(9);  // vincula el servo al pin digital 9
@@ -62,15 +71,17 @@ void setup()
     pinMode(IN2,OUTPUT);          
     pinMode(IN3,OUTPUT);          
     pinMode(IN4,OUTPUT);         
-    analogWrite(VelocidadMotor1, 100); //motor derecho
-    analogWrite(VelocidadMotor2, 100);  //motor izquierdo
 }
 
 void loop()
 {  
     dis = getDistance();
-
+    valorInfraI = digitalRead(infraIZQ);    // valor de la entrada que lee el infrarrojo izquierdo
+    valorInfraD = digitalRead(infraDER);  // derecho
+        
     if(fase2 == false){
+            analogWrite(VelocidadMotor1, 100); //motor derecho
+            analogWrite(VelocidadMotor2, 100);  //motor izquierdo
             Serial.println("FASE 1");  
 
             if((dis < 10 || go == false))
@@ -83,10 +94,15 @@ void loop()
                 ALTO;  
                   
                 if(fase1 == true){
-                  
-                      ok = search1();
-                      ok2 = search2();
-                      ok3 = search3();
+
+                      ok2 = search2(); //GIRA EL SENSOR ULTRA HACIA EL FRENTE
+                      if(ok2 == false){
+                        ok = search1();  //GIRA EL SENSOR ULTRA HACIA LA DERECHA
+                      }
+
+                      if(ok == false){
+                        ok3 = search3(); //GIRA EL SENSOR ULTRA HACIA LA IZQUIERDA
+                      }
               
                       if(ok == true){
                         Serial.print("ENCONTRADO OK1");
@@ -139,7 +155,7 @@ void loop()
                 else{
                   Serial.println("PINZA OK");  
                   delay(2000);
-                  servoA.write(65); // Mueve la pinza derecha
+                  servoA.write(50); // Mueve la pinza derecha
                   delay(2000);
                   fase2 = true;
                 }
@@ -158,32 +174,78 @@ void loop()
     
     
     if(fase2 == true) {
-      Serial.println("FASE 2");  
-        digitalWrite(pinVerde, HIGH);
-        digitalWrite(pinRojo, LOW);
-        GIROD;
-        delay(2000);
-        digitalWrite(pinVerde, LOW);
-        digitalWrite(pinRojo, HIGH);
-        GIROI;
-        delay(1000);
-        digitalWrite(pinVerde, HIGH);
-        digitalWrite(pinRojo, LOW);
-        GIROD;
-        delay(1000);
-        digitalWrite(pinVerde, LOW);
-        digitalWrite(pinRojo, HIGH);
-        GIROD;
-        delay(1000);
-        digitalWrite(pinVerde, HIGH);
-        digitalWrite(pinRojo, LOW);
-        GIROI;
-        delay(1000);
-        digitalWrite(pinVerde, LOW);
-        digitalWrite(pinRojo, HIGH);
-        ADELANTE;            
-        delay(2000);
+        Serial.println("FASE 2"); 
+        analogWrite(VelocidadMotor1, 70); //motor derecho
+        analogWrite(VelocidadMotor2, 70);  //motor izquierdo 
+        
+           if(valorInfraI == 1 || valorInfraD == 1){
+               ALTO;
+               delay(2000);
+               fase2 = false;
+               fase3 = true;   
+               digitalWrite(pinVerde, HIGH);
+               digitalWrite(pinRojo, LOW);
+            }
+            
+            if(dis < 10){
+                ALTO;
+                delay(500);
+                GIROD;
+                delay(2300);;       
+            }
+            else if(dis > 10 && dis < 50){
+                ADELANTE;
+            }
       }
+
+       if(fase3 == true){
+            analogWrite(VelocidadMotor1, 50); //motor derecho
+            analogWrite(VelocidadMotor2, 50);  //motor izquierdo 
+            valorInfraI = digitalRead(infraIZQ);    // valor de la entrada que lee el infrarrojo izquierdo
+            valorInfraD = digitalRead(infraDER);  // derecho
+        
+            digitalWrite(pinVerde, HIGH);
+            digitalWrite(pinRojo, LOW);
+            
+             // 0 = blanco / 1 = negro
+            if(valorInfraI == 0 && valorInfraD == 0){ // Hacia delante
+              ADELANTE;   
+            }
+      
+            if(valorInfraI == 0 && valorInfraD == 1){  // Giro derecha
+              GIROD;
+              delay(200);
+            }
+          
+            if(valorInfraI == 1 && valorInfraD == 0){ // Giro izquierda
+              GIROI;
+              delay(200);
+            }
+          
+            if(valorInfraI == 1 && valorInfraD == 1){  // STOP
+              ALTO;   
+              delay(1000);
+              servoA.write(5); // POSICION INICIAL DEL SERVO PARA LA PINZA
+              delay(1000);
+              ATRAS;
+              delay(2000);
+              GIROI;
+              delay(3000);
+              fase2 = false;
+              fase1 = true;
+              go = false;
+              ok = false;
+              ok2 = false;
+              ok3 = false;
+              fase3 = false;
+            }
+            
+            delay(100);
+            digitalWrite(pinVerde, LOW);
+            digitalWrite(pinRojo, HIGH);
+            delay(100);
+        }
+
 
  }
 
@@ -191,11 +253,6 @@ boolean search1(){
   myservo.write(-20);  ;          
   delay(3000);
   dis = getDistance();
- /* dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();*/
 
   if(dis > 20 && dis < 100){
     Serial.print("ENCONTRADO SEARCH1");
@@ -212,11 +269,6 @@ boolean search2(){
   myservo.write(35);  ;          
   delay(3000);
   dis = getDistance();
- /* dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();*/
 
   if(dis > 20 && dis < 100){
     Serial.print("ENCONTRADO SEARCH2");
@@ -234,11 +286,6 @@ boolean search3(){
   myservo.write(80);  ;          
   delay(3000);
   dis = getDistance();
- /* dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();
-  dis = getDistance();*/
 
   if(dis > 20 && dis < 100){
     Serial.print("ENCONTRADO SEARCH3");
